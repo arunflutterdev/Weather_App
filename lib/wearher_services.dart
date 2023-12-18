a064f7cddf74b503db4321302e28a17b793caa0b
[Arun Pandian] <[arun07160@gmail.com]>
2023-12-18 23:00:33 +0530

    first commit

---
+++ lib/wearher_services.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/weather_model.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService{
+  static const BASE_URL='http://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+  WeatherService(this.apiKey);
+  Future<Weather>getWeather(String cityName)async{
+    final response =await http
+    .get(Uri.parse('$BASE_URL?q=$cityName &appid=$apiKey&units=metric'));
+    if(response.statusCode==200){
+      return Weather.fromJson(jsonDecode(response.body));
+
+    }else{
+      throw Exception('failed to load weather');
+    }
+  }
+
+  Future<String> getCurrentCity() async{
+    //get permission from user
+    LocationPermission permission=await Geolocator.checkPermission();
+    if(permission==LocationPermission.denied){
+      permission=await Geolocator.requestPermission();
+
+    }
+    //Fetch current location
+    Position position=await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    // convert location into place mark
+    List<Placemark>placemark=await placemarkFromCoordinates(position.latitude,position.longitude);
+
+    //city name from first placemark
+    String? city=placemark[0].locality;
+    return city ?? ""; 
+
+
+  }
+}
---
+++ lib/weather_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/wearher_services.dart';
+import 'package:flutter_application_1/weather_model.dart';
+import 'package:lottie/lottie.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  //apikey
+  final _weatherService = WeatherService('7395ad80987e7efc2669fe96d0dc42e1');
+  Weather? _weather;
+
+  //fetch Wearher
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather=weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+//weather animation
+String getWeatherAnimation(String?mainCondition){
+  if (mainCondition==null) return 'assets/sunny.json';
+  switch(mainCondition.toLowerCase()){
+    case 'clouds':
+    case 'mist':
+    case 'smoke':
+    case 'haze':
+    case 'dust':
+    case 'fog':
+    
+    return 'assets/cloudy.json';
+    case 'rain':
+    case 'drizzle':
+    case 'shower rain':
+    return 'assets/rainy.json';
+    case 'thunderstrom':
+    return 'assets/thunder.json';
+    default:
+    return 'assets/sunny.json';
+
+  }
+
+}
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchWeather();
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true,
+      backgroundColor: Colors.black,
+      title: Text("Weather",style: TextStyle(color: Colors.white),),),
+      backgroundColor: Colors.grey.shade900,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Text(_weather?.cityName??"loading city.."
+          ,style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold,color: Colors.white)
+          ),
+          Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+          Text('${_weather?.temperature.round()}°C'
+          ,style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold,color: Colors.white)
+          ),
+          Text(_weather?.mainCondition??"",
+          style: TextStyle(fontSize: 23,fontWeight: FontWeight.bold,color: Colors.white)
+          ,)
+
+      
+        ],),
+      ),
+    );
+  }
+}
